6fc9c25b8248a336350db2d16e62e4f22a08f129
Mohammed Altohami <altohami360@gmail.com>
2023-05-10 14:23:27 +0300

    Update path.blade.php

--- resources/views/components/portfolio/path.blade.php
+++ resources/views/components/portfolio/path.blade.php
@@ -2,7 +2,7 @@
     <div class="py-2">
         <span class="lg:text-base text-sm text-green-500">BACK-END</span>
         <span class="lg:text-base text-sm text-yellow-500">@@dev</span>
-        <span class="lg:text-base text-sm text-white">:$ </span>
+        <span class="lg:text-base text-sm text-white">:# </span>
         <span
             {{ $attributes->merge([
                 'class' => 'lg:text-base text-sm text-red-500 tracking-widest',
