423893f6c1ab858c2d15f9121f28ba02baf0ad4e
Mohammed Altohami <altohami360@gmail.com>
2023-05-11 11:50:23 +0300

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,6 +8,14 @@ class TestController extends Controller
 {
     public function test(Request $request)
     {
-        return $request->header();
+        return [
+            'all' => $request->all(),
+            'host' => $request->host(),
+            'query' => $request->query(),
+            'method' => $request->method(),
+            'json' => $request->json(),
+            'path' => $request->path(),
+            'header' => $request->header(),
+        ];
     }
 }
